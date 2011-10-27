ef718fd36969f38e63f0e885fc2bc197201ba6cd
Toshiyasu Shimizu <toshi0328@gmail.com>
2011-10-27 20:28:36 +0900

    use display list

--- lib/disp3D.rb
+++ lib/disp3D.rb
@@ -18,6 +18,7 @@ require 'manipulator'
 require 'scene_graph'
 require 'node'
 require 'node_collection'
+require 'node_leaf'
 
 require 'node_tea_pod'
 require 'node_points'
--- lib/node.rb
+++ lib/node.rb
@@ -2,24 +2,9 @@ require 'disp3D'
 
 module Disp3D
   class Node
-    attr_accessor :geom
     attr_accessor :translate
-    attr_accessor :material_color
-    attr_accessor :shininess
 
     def pre_draw
-      diffuse = @material_color
-      ambient = [@material_color[0]*0.5, @material_color[1]*0.5, @material_color[2]*0.5, 1]
-      specular = [1,1,1,0.5]
-
-      shineness = [@shiness]
-      shineness = [@shininess_default] if( !@shiness )
-
-      GL.Materialfv(GL::GL_FRONT, GL::GL_DIFFUSE, diffuse)
-      GL.Materialfv(GL::GL_FRONT, GL::GL_AMBIENT, ambient)
-      GL.Materialfv(GL::GL_FRONT, GL::GL_SPECULAR, specular)
-      GL.Materialfv(GL::GL_FRONT, GL::GL_SHININESS, shineness)
-
       GL.PushMatrix()
       GL.Translate(translate[0], translate[1], translate[2]) if(@translate)
     end
@@ -29,13 +14,8 @@ module Disp3D
     end
 
     def initialize(geometry)
-      @geom = geometry
-
+      super
       @translate = nil
-
-      @material_color = [1.0, 1.0, 1.0, 1.0]
-      @shininess = nil
-      @shininess_default = 32.0
     end
   end
 end
