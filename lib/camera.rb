c472e61df643b27afb55cdcd117786608376e231
Toshiyasu Shimizu <toshi0328@gmail.com>
2011-11-14 15:00:01 +0900

    update fit and unprojected method for picking

--- lib/camera.rb
+++ lib/camera.rb
@@ -6,8 +6,6 @@ module Disp3D
     attr_accessor :translate
 
     attr_reader :eye
-    attr_reader :center
-    attr_reader :far
     attr_accessor :scale
 
     attr_accessor :is_orth
@@ -17,11 +15,14 @@ module Disp3D
       @translate = Vector3.new(0,0,0)
       @eye = Vector3.new(0,0,1)
       @center = Vector3.new(0,0,0)
-      @scale = 1
+      @scale = 1.0
+
+      @obj_rep_length = 10.0
+
       @angle = 30
-      @far = 100.0
 
       @is_orth = false
+      @orth_scale = 1.0
     end
 
     def reshape(w,h)
@@ -53,9 +54,10 @@ module Disp3D
 
     def set_screen(w,h)
       if @is_orth
-        GL.Ortho(-w/2.0, w/2.0, -h/2.0, h/2.0, -@far*@scale*10, @far*@scale*10)
+        p "rep_length:#{@obj_rep_length}"
+        GL.Ortho(-w*@orth_scale/2.0, w*@orth_scale/2.0, -h*@orth_scale/2.0, h*@orth_scale/2.0, -@obj_rep_length*10, @obj_rep_length*10)
       else
-        GLU.Perspective(@angle, w.to_f()/h.to_f(), 0.1, @far)
+        GLU.Perspective(@angle, w.to_f()/h.to_f(), 0.1, @eye.z + @obj_rep_length*5.0)
       end
     end
 
@@ -64,47 +66,36 @@ module Disp3D
     end
 
     def unproject(screen_pos)
-      vp = GL.GetIntegerv(GL::VIEWPORT)
+      vp = viewport
       projection = GL::GetDoublev(GL::PROJECTION_MATRIX)
       model_view = GL::GetDoublev(GL::MODELVIEW_MATRIX)
-
-      unprojected = GLU::UnProject(screen_pos.x,vp[3]-screen_pos.y-1,screen_pos.z, model_view, projection, vp)
-      unprojected[2] += @eye.z
+      unprojected = GLU::UnProject(screen_pos.x, vp[3]-screen_pos.y-1, screen_pos.z, model_view, projection, vp)
       unprojected = Vector3.new(unprojected[0], unprojected[1], unprojected[2])
+      unprojected.z += @eye.z
 
-      if @is_orth
-        w = vp[2]
-        h = vp[3]
-        min_screen_size = [w, h].min
-
-# TODO Refactoring!!
-# something is Wrong.. in orth
-        rad = @eye.z/((Math.sqrt(w*w+h*h)/min_screen_size)/(Math.tan(@angle/2.0*Math::PI/180.0)))
-        current_scale = (min_screen_size.to_f/2.0)/rad
-      else
-        current_scale = @scale
-      end
       unprojected -= @translate
       rot_matrix = Matrix.from_quat(@rotation)
       unprojected = rot_matrix*unprojected
-      unprojected /= (current_scale)
+      unprojected /= @scale
       return unprojected
     end
 
-    def fit(radius)
+    def fit_camera_pos
+      # move camera pos for showing all objects
       dmy, dmy, w, h = viewport
       min_screen_size = [w, h].min
-      eye_z = radius*(Math.sqrt(w*w+h*h)/min_screen_size)/(Math.tan(@angle/2.0*Math::PI/180.0))
+      eye_z = @obj_rep_length*(Math.sqrt(w*w+h*h)/min_screen_size.to_f)/(Math.tan(@angle/2.0*Math::PI/180.0))
       @eye = Vector3.new(0,0,eye_z)
-      @far = eye_z + radius*2
-      if @is_orth
-        @scale = (min_screen_size.to_f/2.0)/radius
-      else
-        @scale = 1.0
-      end
-      set_screen(w,h)
+      @orth_scale = @obj_rep_length/(min_screen_size.to_f)*2.0
     end
 
+    def fit(radius)
+      @obj_rep_length = radius
+      fit_camera_pos
+      @scale = 1.0
+      dmy, dmy, w, h = viewport
+      set_screen(w,h)
+    end
 
 =begin
     def screen_size_at_z_zero()
