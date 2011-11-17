aa4bcbfad2b117170ae878f15f25a5ee042159cc
Toshiyasu Shimizu <toshi0328@gmail.com>
2011-11-17 09:09:53 +0900

    added STL viewing test

---
+++ example/test/stl_test.rb
@@ -0,0 +1,23 @@
+require 'opengl'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'lib'))
+
+require 'disp3D'
+
+main_view = Disp3D::GLUTWindow.new(600,400)
+root_node = Disp3D::NodeCollection.new()
+
+stl = Disp3D::STL.new();
+file_path = File.dirname(__FILE__) + "/../../test/data/bunny.stl";
+
+if( !File.exists?(file_path) )
+  p file_path + " is not exist!"
+  exit
+end
+stl.parse(file_path, Disp3D::STL::ASCII)
+stl_node = Disp3D::NodeTris.new( stl.tri_mesh )
+
+main_view.world_scene_graph.add(stl_node)
+
+#main_view.camera.is_orth = true
+main_view.start
