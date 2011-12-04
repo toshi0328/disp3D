29255a581f7e4c70778074e80f9dae6a9e8b2bff
Toshiyasu Shimizu <toshi0328@gmail.com>
2011-12-04 22:39:55 +0900

    update workplane

--- example/tutorial/12_Light.rb
+++ example/tutorial/12_Light.rb
@@ -3,14 +3,20 @@ require 'disp3D'
 
 main_view = Disp3D::GLUTWindow.new(400,400,"10_Animation")
 
-light_pos_ary = [Vector3.new(30,0,0), Vector3.new(-30,0,0), Vector3.new(0,0,30)]
-light_spot_dir_ary = [Vector3.new(-1,0,0), Vector3.new(1,0,0), Vector3.new(0,0,-1)]
+light_pos_ary = [Vector3.new(30,0,0), Vector3.new(-30,0,0), Vector3.new(0,0,30), Vector3.new(0,5,0)]
+light_spot_dir_ary = [Vector3.new(-1,0,0), Vector3.new(1,0,0), Vector3.new(0,0,-1), Vector3.new(0,-1,0)]
 
 main_view.world_scene_graph.open do
   add_new :type => :TeaPod,
+          :name => :pod,
           :material_color => [1,1,1,1],
-          :size => 10.0,
-          :name => :pod
+          :size => 10.0
+
+  add_new :type => :Workplane,
+          :name => :workplane,
+          :geom => Plane.new(Vector3.new(0,-8,0), Vector3.new(0,1,0)),
+          :material_color => [0.8,0.8,0.2,0.5],
+          :grid => 10
 
   light_pos_ary.each_with_index do |light_pos,idx|
     add_new :type => :Cone,
@@ -43,6 +49,14 @@ main_view.light.open do
       :specular => [0, 0, 1, 1],
       :position => light_pos_ary[2],
       :spot_direction => light_spot_dir_ary[2]
+
+  set :id => 3,
+      :enable => true,
+      :diffuse => [0.7, 0.7, 0.7, 1],
+      :ambient => [0.2, 0.2, 0.2, 1],
+      :specular => [1, 1, 1, 1],
+      :position => light_pos_ary[3],
+      :spot_direction => light_spot_dir_ary[3]
 end
 
 main_view.camera.projection = Disp3D::Camera::ORTHOGONAL
--- lib/node/node_workplane.rb
+++ lib/node/node_workplane.rb
@@ -3,12 +3,14 @@ require 'disp3D'
 module Disp3D
   class NodeWorkplane < NodeLeaf
     attr_for_disp :length
+    attr_for_disp :grid
 
     def initialize(geom, name = nil)
       Util3D.check_arg_type(Symbol, name, true)
       Util3D.check_arg_type(GMath3D::Plane, geom, false)
       super
       @length = 1000
+      @grid = nil
     end
 
     def box
@@ -25,12 +27,42 @@ protected
           GL.Vertex( vertex.x, vertex.y, vertex.z )
         end
         GL.End()
+
+        GL.Color(0,0,0,1)
+        GL.LineWidth(3)
+        if(!@grid.nil?)
+          line_vertices_ary = grid_lines_vertices
+          GL.Begin(GL::LINES)
+          line_vertices_ary.each do |vertex|
+            GL.Vertex( vertex.x, vertex.y, vertex.z )
+          end
+          GL.End()
+        end
       else
         raise
       end
     end
 
 private
+    def grid_lines_vertices
+      count = (@length/@grid) + 1
+      u_vec = @geom.normal
+      u_vec = @geom.normal.arbitrary_orthogonal
+      v_vec = @geom.normal.cross(u_vec)
+      vertices = Array.new(count*4)
+      count.times.each do |idx|
+        point_tmp = @geom.base_point + u_vec * @grid * (idx - count/2)
+        vertices[2*idx] = point_tmp + v_vec * @length/2.0
+        vertices[2*idx+1] = point_tmp - v_vec * @length/2.0
+      end
+      count.times.each do |idx|
+        point_tmp = @geom.base_point + v_vec * @grid * (idx - count/2)
+        vertices[2*idx+2*count] = point_tmp + u_vec * @length/2.0
+        vertices[2*idx+2*count+1] = point_tmp - u_vec * @length/2.0
+      end
+      return vertices
+    end
+
     def calc_vertices
       tan_vec1 = @geom.normal.arbitrary_orthogonal
       tan_vec2 = @geom.normal.cross(tan_vec1)
