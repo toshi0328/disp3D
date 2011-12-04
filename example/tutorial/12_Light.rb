27bc7314e36cdd79cd9cd5f13985d06b063963e8
Toshiyasu Shimizu <toshi0328@gmail.com>
2011-12-04 21:10:06 +0900

    update lighting

--- example/tutorial/12_Light.rb
+++ example/tutorial/12_Light.rb
@@ -3,7 +3,8 @@ require 'disp3D'
 
 main_view = Disp3D::GLUTWindow.new(400,400,"10_Animation")
 
-light_pos_ary = [Vector3.new(30,0,0), Vector3.new(-30,0,0)]
+light_pos_ary = [Vector3.new(30,0,0), Vector3.new(-30,0,0), Vector3.new(0,0,30)]
+light_spot_dir_ary = [Vector3.new(-1,0,0), Vector3.new(1,0,0), Vector3.new(0,0,-1)]
 
 main_view.world_scene_graph.open do
   add_new :type => :TeaPod,
@@ -21,17 +22,27 @@ end
 main_view.light.open do
   set :id => 0,
       :enable => true,
-      :diffuse => [0.7, 0, 0, 1],
-      :ambient => [0.2, 0, 0, 1],
-      :specular => [1, 0, 0, 1],
-      :position => light_pos_ary[0]
-
-  set :id => 1,
-      :enable => true,
       :diffuse => [0, 0.7, 0, 1],
       :ambient => [0, 0.2, 0, 1],
       :specular => [0, 1, 0, 1],
-      :position => light_pos_ary[1]
+      :position => light_pos_ary[0],
+      :spot_direction => light_spot_dir_ary[0]
+
+  set :id => 1,
+      :enable => true,
+      :diffuse => [0.7, 0, 0, 1],
+      :ambient => [0.2, 0, 0, 1],
+      :specular => [1, 0, 0, 1],
+      :position => light_pos_ary[1],
+      :spot_direction => light_spot_dir_ary[1]
+
+  set :id => 2,
+      :enable => true,
+      :diffuse => [0, 0, 0.7, 1],
+      :ambient => [0, 0, 0.2, 1],
+      :specular => [0, 0, 1, 1],
+      :position => light_pos_ary[2],
+      :spot_direction => light_spot_dir_ary[2]
 end
 
 main_view.camera.projection = Disp3D::Camera::ORTHOGONAL
--- lib/light.rb
+++ lib/light.rb
@@ -7,12 +7,14 @@ module Disp3D
      attr_accessor :ambient
      attr_accessor :specular
      attr_accessor :position
+     attr_accessor :spot_direction
 
     def initialize()
       @diffuse_default = [0.7, 0.7, 0.7, 1]
       @ambient_default = [0.2, 0.2, 0.2, 1]
       @specular_default = [1, 1, 1, 1]
       @position_default = Vector3.new(0.0, 0.0, 1.0)
+      @spot_direction_default = Vector3.new(0.0, 0.0, -1.0)
 
       light_count = 8 # openGL spec
 
@@ -21,6 +23,7 @@ module Disp3D
       @ambient = Array.new(light_count, @ambient_default)
       @specular = Array.new(light_count, @specular_default)
       @position = Array.new(light_count, @position_default)
+      @spot_direction = Array.new(light_count, @spot_direction_default)
       @light_id = Array.new(light_count)
       @light_id[0] = GL::GL_LIGHT0
       @light_id[1] = GL::GL_LIGHT1
@@ -37,10 +40,13 @@ module Disp3D
       @enable.each_with_index do | enable, idx |
         if(enable)
           GL.Enable( @light_id[idx])
-          GL.Lightfv( @light_id[idx], GL::GL_POSITION, [@position[idx].x,@position[idx].y,@position[idx].z] )
-          GL.Lightfv( @light_id[idx], GL::GL_DIFFUSE,  @diffuse[idx]  )
-          GL.Lightfv( @light_id[idx], GL::GL_AMBIENT,  @ambient[idx]  )
-          GL.Lightfv( @light_id[idx], GL::GL_SPECULAR, @specular[idx] )
+#p @position[idx]
+#p @spot_direction[idx]
+          GL.Lightfv( @light_id[idx], GL::POSITION, [@position[idx].x,@position[idx].y,@position[idx].z] )
+          GL.Lightfv( @light_id[idx], GL::SPOT_DIRECTION, [@spot_direction[idx].x,@spot_direction[idx].y,@spot_direction[idx].z] )
+          GL.Lightfv( @light_id[idx], GL::DIFFUSE,  @diffuse[idx]  )
+          GL.Lightfv( @light_id[idx], GL::AMBIENT,  @ambient[idx]  )
+          GL.Lightfv( @light_id[idx], GL::SPECULAR, @specular[idx] )
         end
       end
       if(!@enable.include?(true))
